66ee089bf313e811b92d24f0cd02a49a9007978c
Egor <777egorkalinin777@gmail.com>
2022-11-12 11:59:19 +0300

    Add main sheets

--- everythingbeats/views.py
+++ everythingbeats/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 
 def main_everythingbeats(request):
@@ -8,8 +10,17 @@ def main_everythingbeats(request):
 
 
 def signupuser(request):
-    return render(request, 'everythingbeats/signupuser.html', {'form': UserCreationForm()})
-    # if requst.method == 'GET':
-    #     return render(request, 'everythingbeats/signupuser.html', {'form': UserCreationForm()})
-    # else:
-    #     User.objects.create_user(requst.POST[])
+    #  return render(request, 'everythingbeats/signupuser.html', {'form': UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'everythingbeats/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('main_everythingbeats')
+            except IntegrityError:
+                return render(request, 'everythingbeats/signupuser.html', {'form': UserCreationForm(), 'error': 'That username has already taken. Please choose a new one.'})
+        else:
+            return render(request, 'everythingbeats/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords did not match'})
